61bce240204f3c03796afd0552492ebd3b7e190e
Tomasz Giela <chrono5@wp.pl>
2020-05-26 21:27:30 +0200

    Fix counter bug

--- SortingComparison/SortingAlgorithms/InsertionSort.cs
+++ SortingComparison/SortingAlgorithms/InsertionSort.cs
@@ -1,20 +1,26 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using System.Numerics;
 
 namespace SortingComparison
 {
     class InsertionSort : ISorter
     {
+        public NumberProvider NumberProvider { get; set; }
         public string SortAlgorithmName { get; set; } = "Insertion Sort";
+
+        public InsertionSort()
+        {
+            NumberProvider = new NumberProvider();
+        }
 
         public BigInteger Sort(int[] numbers)
         {
-            BigInteger totalNumberOfComparisons = 0;
+            BigInteger totalNumberOfComparisons = 1;
 
             int n = numbers.Length;
             for (int i = 1; i < n; ++i)
             {
+                totalNumberOfComparisons++;
                 int key = numbers[i];
                 int j = i - 1;
                 while (j >= 0 && numbers[j] > key)
@@ -30,11 +36,12 @@ namespace SortingComparison
 
         public BigInteger Sort(List<int> numbers)
         {
-            BigInteger totalNumberOfComparisons = 0;
+            BigInteger totalNumberOfComparisons = 1;
 
             int n = numbers.Count;
             for (int i = 1; i < n; ++i)
             {
+                totalNumberOfComparisons++;
                 int key = numbers[i];
                 int j = i - 1;
                 while (j >= 0 && numbers[j] > key)
