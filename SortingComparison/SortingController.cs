7e3d43d769121b1ada2c0fc26c92410e6390fa26
Tomasz Giela <chrono5@wp.pl>
2020-05-26 21:29:27 +0200

    Remove NumberProvider from SortingController

--- SortingComparison/SortingController.cs
+++ SortingComparison/SortingController.cs
@@ -6,12 +6,10 @@ namespace SortingComparison
 {
     class SortingController
     {
-        private readonly NumberProvider numberProvider;
         private readonly List<ISorter> algorithms;
 
         public SortingController(List<ISorter> algorithms)
         {
-            this.numberProvider = LoadNumbersFromFile();
             this.algorithms = algorithms;
         }
 
@@ -19,7 +17,7 @@ namespace SortingComparison
         {
             foreach (var algorithm in algorithms)
             {
-                for (int i = 0; i < numberProvider.ArraysOfUnorderedNumbers.Count; i++)
+                for (int i = 0; i < algorithm.NumberProvider.ArraysOfUnorderedNumbers.Count; i++)
                 {
                     Sort(algorithm, i);
                 }
@@ -31,23 +29,14 @@ namespace SortingComparison
             //Timed sorting
             Stopwatch benchmarkTime = new Stopwatch();
             benchmarkTime.Start();
-            var totalNumberOfComparisons = Sorter.Sort(numberProvider.ArraysOfUnorderedNumbers[fileNumber]);
+            var totalNumberOfComparisons = Sorter.Sort(Sorter.NumberProvider.ArraysOfUnorderedNumbers[fileNumber]);
             benchmarkTime.Stop();
             TimeSpan ts = benchmarkTime.Elapsed;
             string elapsedTime = $"{ts.TotalMilliseconds} miliseconds";
             //Results printout
-            Console.WriteLine($"{Sorter.SortAlgorithmName} on {numberProvider.ShuffledFilePaths[fileNumber]}");
+            Console.WriteLine($"{Sorter.SortAlgorithmName} on {Sorter.NumberProvider.ShuffledFilePaths[fileNumber]}");
             Console.WriteLine(elapsedTime);
             Console.WriteLine($"{totalNumberOfComparisons} comparisons");
-        }
-
-        private NumberProvider LoadNumbersFromFile()
-        {
-            Console.WriteLine("Hello in Sorting Comparison");
-            NumberProvider NumberProvider = new NumberProvider();
-            Console.WriteLine("Completed Loading Files");
-            Console.WriteLine(Environment.NewLine);
-            return NumberProvider;
         }
     }
 }
