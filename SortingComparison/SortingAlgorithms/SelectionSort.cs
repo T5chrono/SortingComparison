810e56d1cb9248a165974a2fcfb4d727e6efce9a
Tomasz Giela <chrono5@wp.pl>
2020-05-27 20:56:21 +0200

    Implement SelectionSort

--- SortingComparison/SortingAlgorithms/SelectionSort.cs
+++ SortingComparison/SortingAlgorithms/SelectionSort.cs
@@ -1,10 +1,62 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Numerics;
 using System.Text;
 
 namespace SortingComparison
 {
-    class SelectionSort
+    class SelectionSort : ISorter
     {
+        public NumberProvider NumberProvider { get; set; }
+        public string SortAlgorithmName { get; set; } = "Selection Sort";
+
+        public SelectionSort()
+        {
+            NumberProvider = new NumberProvider();
+        }
+        public SelectionSort(NumberProvider originalNumberProvider)
+        {
+            this.NumberProvider = new NumberProvider(originalNumberProvider);
+        }
+
+        public BigInteger Sort(int[] numbers, int start = 0, int end = 0)
+        {
+            BigInteger totalNumberOfComparisons = 0;
+
+            int n = numbers.Length;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                int min_idx = i;
+                for (int j = i + 1; j < n; j++)
+                    if (numbers[j] < numbers[min_idx])
+                        min_idx = j;
+
+                int temp = numbers[min_idx];
+                numbers[min_idx] = numbers[i];
+                numbers[i] = temp;
+            }
+            return totalNumberOfComparisons;
+        }
+
+        public BigInteger Sort(List<int> numbers, int start = 0, int end = 0)
+        {
+            BigInteger totalNumberOfComparisons = 0;
+
+            int n = numbers.Count;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                int min_idx = i;
+                for (int j = i + 1; j < n; j++)
+                    if (numbers[j] < numbers[min_idx])
+                        min_idx = j;
+
+                int temp = numbers[min_idx];
+                numbers[min_idx] = numbers[i];
+                numbers[i] = temp;
+            }
+            return totalNumberOfComparisons;
+        }
     }
 }
